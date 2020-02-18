278e5ff7c1e37263631d61e07c3b77babcaadf83
Shubham <196shubhamjadhav@gmail.com>
2020-02-18 19:19:54 +0530

    Added Project

---
+++ WomenEmpowerment/src/main/java/com/lti/wp/dao/StepRegDaoImpl.java
@@ -0,0 +1,40 @@
+package com.lti.wp.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lti.wp.entities.StepRegister;
+import com.lti.wp.exceptions.WpException;
+
+@Repository
+public class StepRegDaoImpl implements StepRegDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public ArrayList<StepRegister> getStepRegister() throws WpException {
+		String strQry = "from StepRegister";
+		Query qry = manager.createQuery(strQry);
+		List<StepRegister> list = qry.getResultList();
+		return (ArrayList<StepRegister>) list;
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRED)
+	@Override
+	public boolean postStepRegister(StepRegister reg) throws WpException {
+		manager.persist(reg);
+		return true;
+	}
+
+	
+
+
+}
---
+++ WomenEmpowerment/src/main/java/com/lti/wp/exceptions/WpException.java
@@ -0,0 +1,14 @@
+package com.lti.wp.exceptions;
+
+public class WpException extends Exception {
+
+	public WpException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public WpException(String arg0) {
+		super(arg0);
+	}
+
+	
+}
